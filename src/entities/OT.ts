a1f79e6b8f94ab7cc1b5ed2103b73067c7351acc
Jin Heng Yap <jinhengyap@gmail.com>
2022-04-13 17:18:20 +0800

    Add OT + YT

--- src/entities/OT.ts
+++ src/entities/OT.ts
@@ -1,25 +1,38 @@
+import type { PendleOwnershipToken } from '@pendle/core-v2/typechain-types';
 import type { Address, NetworkConnection } from './types';
 import type { UserYOInfo, YOInfo } from './YT';
+import { YT } from './YT';
 import { Contract } from 'ethers';
 import { dummyABI } from '../dummy';
+
 export class OT {
   public address: Address;
-  public contract: Contract; // To-Be replaced by typechain class
+  public contract: PendleOwnershipToken;
   public chainId: number;
+  public yt?: YT;
 
   protected networkConnection: NetworkConnection;
+
   public constructor(_address: Address, _networkConnection: NetworkConnection, _chainId: number) {
     this.address = _address;
     this.networkConnection = _networkConnection;
     this.chainId = _chainId;
-    this.contract = new Contract(_address, dummyABI, _networkConnection.provider);
+    this.contract = new Contract(_address, dummyABI, _networkConnection.provider) as PendleOwnershipToken;
   }
 
-  userInfo(user: Address): UserYOInfo {
-    return '' as unknown as UserYOInfo;
+  async getYT(): Promise<YT> {
+    if (!this.yt) {
+      const ytAddress = await this.contract.callStatic.YT();
+      this.yt = new YT(ytAddress, this.networkConnection, this.chainId);
+    }
+    return this.yt;
   }
 
-  getInfo(): YOInfo {
-    return {} as unknown as YOInfo;
+  async userInfo(user: Address): Promise<UserYOInfo> {
+    return (await this.getYT()).userInfo(user);
+  }
+
+  async getInfo(): Promise<YOInfo> {
+    return (await this.getYT()).getInfo();
   }
 }
--- src/entities/YT.ts
+++ src/entities/YT.ts
@@ -1,5 +1,7 @@
+import type { PendleYieldToken } from '@pendle/core-v2/typechain-types';
 import type { Address, NetworkConnection, TokenAmount } from './types';
 import { type BigNumber as BN, Contract } from 'ethers';
+import { OT } from './OT';
 import { dummyABI } from '../dummy';
 
 export type UserYOInfo = {
@@ -14,27 +16,60 @@ export type UserYOInfo = {
 export type YOInfo = {
   exchangeRate: BN;
   totalSupply: BN;
-  totalInterest: BN;
+  rewardIndexes: AddressAndIndex[];
+};
+
+export type AddressAndIndex = {
+  address: Address;
+  index: BN;
 };
 
 export class YT {
   public address: Address;
-  public contract: Contract; // To-Be replaced by typechain class
+  public contract: PendleYieldToken;
   public chainId: number;
 
   protected networkConnection: NetworkConnection;
+
   public constructor(_address: Address, _networkConnection: NetworkConnection, _chainId: number) {
     this.address = _address;
     this.networkConnection = _networkConnection;
     this.chainId = _chainId;
-    this.contract = new Contract(_address, dummyABI, _networkConnection.provider);
+    this.contract = new Contract(_address, dummyABI, _networkConnection.provider) as PendleYieldToken;
   }
 
-  userInfo(user: Address): UserYOInfo {
-    return '' as unknown as UserYOInfo;
+  async userInfo(user: Address): Promise<UserYOInfo> {
+    const ytAddress = this.address;
+    const [ytBalance, otAddress, scyAddress, { dueInterest }, rewardTokens] = await Promise.all([
+      this.contract.callStatic.balanceOf(user),
+      this.contract.callStatic.OT(),
+      this.contract.callStatic.SCY(),
+      this.contract.callStatic.data(user),
+      this.contract.callStatic.getRewardTokens(),
+    ]);
+    const unclaimedInterest = { token: scyAddress, amount: dueInterest };
+    const getRewardBalance = async (token: Address): Promise<TokenAmount> => ({
+      token,
+      amount: (await this.contract.callStatic.userReward(user, token)).accruedReward,
+    });
+    const [otBalance, unclaimedRewards] = await Promise.all([
+      new OT(otAddress, this.networkConnection, this.chainId).contract.callStatic.balanceOf(user),
+      Promise.all(rewardTokens.map(getRewardBalance)),
+    ]);
+    return { ytAddress, ytBalance, otAddress, otBalance, unclaimedInterest, unclaimedRewards };
   }
 
-  getInfo(): YOInfo {
-    return {} as unknown as YOInfo;
+  async getInfo(): Promise<YOInfo> {
+    const [exchangeRate, totalSupply, rewardTokens] = await Promise.all([
+      this.contract.callStatic.getScyIndexBeforeExpiry(),
+      this.contract.callStatic.totalSupply(),
+      this.contract.callStatic.getRewardTokens(),
+    ]);
+    const getRewardIndex = async (token: Address): Promise<AddressAndIndex> => ({
+      address: token,
+      index: (await this.contract.callStatic.globalReward(token)).index,
+    });
+    const rewardIndexes = await Promise.all(rewardTokens.map(getRewardIndex));
+    return { exchangeRate, totalSupply, rewardIndexes };
   }
 }
