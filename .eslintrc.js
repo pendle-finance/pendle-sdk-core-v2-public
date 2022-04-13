3ce3e247cbd41463fd0182eb2b463b7ec165cce3
Hanabi-R <hana.rei.andata@gmail.com>
2022-04-13 08:06:48 +0000

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error'],
-    indent: ['error', 2, { SwitchCase: 1 }],
+    indent: ['error', 4],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
