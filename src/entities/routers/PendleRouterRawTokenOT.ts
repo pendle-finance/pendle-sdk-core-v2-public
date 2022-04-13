e59eba6566da36355ad04427c7c959a34fe5e71d
Hanabi-R <hana.rei.andata@gmail.com>
2022-04-13 08:11:50 +0000

    formatting

--- src/entities/routers/PendleRouterRawTokenOT.ts
+++ src/entities/routers/PendleRouterRawTokenOT.ts
@@ -2,17 +2,17 @@ import { Address, NetworkConnection } from '../types';
 import { Contract } from 'ethers';
 import { dummyABI } from '../../dummy';
 export class PendleRouterRawTokenOT {
-  public address: Address;
-  public contract: Contract; // To-Be replaced by typechain class
-  public chainId: number;
+    public address: Address;
+    public contract: Contract; // To-Be replaced by typechain class
+    public chainId: number;
 
-  protected networkConnection: NetworkConnection;
-  public constructor(_address: Address, _networkConnection: NetworkConnection, _chainId: number) {
-    this.address = _address;
-    this.networkConnection = _networkConnection;
-    this.chainId = _chainId;
-    this.contract = new Contract(_address, dummyABI, _networkConnection.provider);
-  }
+    protected networkConnection: NetworkConnection;
+    public constructor(_address: Address, _networkConnection: NetworkConnection, _chainId: number) {
+        this.address = _address;
+        this.networkConnection = _networkConnection;
+        this.chainId = _chainId;
+        this.contract = new Contract(_address, dummyABI, _networkConnection.provider);
+    }
 
-  // Add additional functions below
+    // Add additional functions below
 }
