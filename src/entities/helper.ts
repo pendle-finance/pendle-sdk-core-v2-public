6e76195fd771506f22c085918f0446a3793b92bf
Jin Heng Yap <jinhengyap@gmail.com>
2022-04-19 22:23:40 +0800

    Correct spelling

--- src/entities/helper.ts
+++ src/entities/helper.ts
@@ -9,14 +9,14 @@ export function decimalFactor(decimals: number): string {
     return BN.from(10).pow(decimals).toString();
 }
 
-export function calcSlippedDownAmount(theoriticalAmount: BN, slippage: number): BN {
-    return theoriticalAmount
+export function calcSlippedDownAmount(theoreticalAmount: BN, slippage: number): BN {
+    return theoreticalAmount
         .mul(BN.from(decimalFactor(PERCENTAGE_DECIMALS)).sub(Math.trunc(slippage * Math.pow(10, PERCENTAGE_DECIMALS))))
         .div(decimalFactor(PERCENTAGE_DECIMALS));
 }
 
-export function calcSlippedUpAmount(theoriticalAmount: BN, slippage: number): BN {
-    return theoriticalAmount
+export function calcSlippedUpAmount(theoreticalAmount: BN, slippage: number): BN {
+    return theoreticalAmount
         .mul(BN.from(decimalFactor(PERCENTAGE_DECIMALS)).add(Math.trunc(slippage * Math.pow(10, PERCENTAGE_DECIMALS))))
         .div(decimalFactor(PERCENTAGE_DECIMALS));
 }
