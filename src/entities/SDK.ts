5b920d19de712ce390e08b0cfdf41179cdd701ca
Jin Heng Yap <jinhengyap@gmail.com>
2022-04-19 14:32:42 +0800

    Add SDK global functions

--- src/entities/SDK.ts
+++ src/entities/SDK.ts
@@ -1,8 +1,32 @@
-// TODO: Fully WIP, feel free to rewrite this
-import type { Address } from './types';
+import type { RouterStatic } from '@pendle/core-v2/typechain-types';
+import type { Address, NetworkConnection } from './types';
 import type { UserYOInfo } from './YT';
 import type { UserMarketInfo } from './Market';
+import { getRouterStatic } from './helper';
 
-export async function getAllOt(): Promise<Address[]> {
-    return [];
+export class SDK {
+    public chainId: number;
+
+    protected networkConnection: NetworkConnection;
+    protected routerStatic: RouterStatic;
+
+    public constructor(_networkConnection: NetworkConnection, _chainId: number) {
+        this.networkConnection = _networkConnection;
+        this.chainId = _chainId;
+        this.routerStatic = getRouterStatic(_networkConnection.provider, _chainId);
+    }
+
+    /**
+     * Get information about the user's YO positions
+     * @param user Address of the user
+     * @param tokens YT, OT, and LP token addresses that we want to check
+     * @returns UserYOInfo object array representing user's YO positions
+     */
+    async getUserYOPositionsByTokens(user: Address, tokens: Address[]): Promise<UserYOInfo[]> {
+        return this.routerStatic.callStatic.getUserYOPositionsByTokens(user, tokens);
+    }
+
+    async getUserMarketPositions(user: Address, markets: Address[]): Promise<UserMarketInfo[]> {
+        return this.routerStatic.callStatic.getUserMarketPositions(user, markets);
+    }
 }
