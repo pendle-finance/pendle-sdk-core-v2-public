cf8d1761e6c6d22f098513986e4016bbcdd915d7
Jin Heng Yap <jinhengyap@gmail.com>
2022-07-14 10:40:17 +0800

    Add contract addresses

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,9 @@ import MUMBAI_CORE_ADDRESSES from '@pendle/core-v2/deployments/80001-core.json';
 export type ContractAddresses = {
     ROUTER: Address;
     ROUTER_STATIC: Address;
+    PENDLE: Address;
+    VEPENDLE: Address;
+    VOTING_CONTROLLER?: Address;
 };
 
 export const CHAIN_ID = {
@@ -18,6 +21,8 @@ export const CHAIN_ID = {
 export const ETHEREUM_ADDRESSES: ContractAddresses = {
     ROUTER: '0xRouter',
     ROUTER_STATIC: '0xRouterStatic',
+    PENDLE: '0xPendle',
+    VEPENDLE: '0xVEPENDLE',
 };
 
 export const AVALANCHE_ADDRESSES: ContractAddresses = ETHEREUM_ADDRESSES;
@@ -25,9 +30,14 @@ export const AVALANCHE_ADDRESSES: ContractAddresses = ETHEREUM_ADDRESSES;
 export const FUJI_ADDRESSES: ContractAddresses = {
     ROUTER: FUJI_CORE_ADDRESSES.router,
     ROUTER_STATIC: FUJI_CORE_ADDRESSES.routerStatic,
+    PENDLE: FUJI_CORE_ADDRESSES.PENDLE,
+    VEPENDLE: FUJI_CORE_ADDRESSES.vePendle,
+    VOTING_CONTROLLER: FUJI_CORE_ADDRESSES.votingController,
 };
 
 export const MUMBAI_ADDRESSES: ContractAddresses = {
     ROUTER: MUMBAI_CORE_ADDRESSES.router,
     ROUTER_STATIC: MUMBAI_CORE_ADDRESSES.routerStatic,
+    PENDLE: MUMBAI_CORE_ADDRESSES.PENDLE,
+    VEPENDLE: MUMBAI_CORE_ADDRESSES.vePendle,
 };
