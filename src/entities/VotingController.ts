acb4e21f1b56a86dad1aaf57e680ac6121858ab6
Jin Heng Yap <jinhengyap@gmail.com>
2022-07-21 10:42:53 +0800

    Refactor VotingController

--- src/entities/VotingController.ts
+++ src/entities/VotingController.ts
@@ -1,7 +1,7 @@
 import type { PendleVotingControllerUpg } from '@pendle/core-v2/typechain-types';
 import type { Address, NetworkConnection } from './types';
 import { BigNumber } from 'bignumber.js';
-import { BigNumber as BN, Contract, constants } from 'ethers';
+import { type Overrides, BigNumber as BN, Contract, constants } from 'ethers';
 import { abi as PendleVotingControllerUpgABI } from '@pendle/core-v2/build/artifacts/contracts/core/LiquidityMining/VotingController/PendleVotingControllerUpg.sol/PendleVotingControllerUpg.json';
 import { isMainchain } from './helper';
 import { Market } from './Market';
@@ -10,9 +10,9 @@ export class VotingController {
     readonly contract: PendleVotingControllerUpg;
 
     constructor(
-        public readonly address: Address,
-        public readonly networkConnection: NetworkConnection,
-        public readonly chainId: number
+        readonly address: Address,
+        protected readonly networkConnection: NetworkConnection,
+        readonly chainId: number
     ) {
         if (!isMainchain(chainId)) {
             throw Error('Voting only available on main chain (Ethereum)');
@@ -25,32 +25,21 @@ export class VotingController {
     }
 
     static scaleWeight(weight: number): BN {
-        if (weight < 0 || weight > 1) {
-            throw new Error('Weight must be in range [0, 1]');
-        }
+        if (weight < 0 || weight > 1) throw new Error('Weight must be in range [0, 1]');
         return BN.from(new BigNumber(constants.WeiPerEther.toString()).times(weight).toFixed());
     }
 
+    // TODO: Uncomment this after the relevant view function is written
     // async getUserTotalVotedWeight(user: Address): Promise<number> {
-    //     // TODO: Uncomment this after interface update
     //     const totalVotedWeight = await this.contract.callStatic.userData(user);
-    //     // const totalVotedWeight = 0;
     //     return new BigNumber(totalVotedWeight.toString()).div(constants.WeiPerEther.toString()).toNumber();
     // }
 
-    async vote(market: Market, weight: number) {
-        return this.contract.vote([market.address], [VotingController.scaleWeight(weight)]);
+    async vote(votes: { market: Market; weight: number }[], overrides: Overrides = {}) {
+        return this.contract.connect(this.networkConnection.signer!).vote(
+            votes.map(({ market }) => market.address),
+            votes.map(({ weight }) => VotingController.scaleWeight(weight)),
+            overrides
+        );
     }
-
-    // async unvote(market: Market) {
-    //     return this.contract.unvote(market.address);
-    // }
-
-    // async updatePoolVotes(market: Market) {
-    //     return this.contract.updatePoolVotes(market.address);
-    // }
-
-    // async voteForMultiple(votes: { market: Market; weight: number }[]) {
-    //     // TODO: Implement this
-    // }
 }
