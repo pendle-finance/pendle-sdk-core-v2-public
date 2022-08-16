e1e9b6c6727ebc90e45726d171d23e3d4d6a185f
Tran Quang Loc <quangloctran99@gmail.com>
2022-08-16 10:47:50 +0800

    Fix lint.

--- src/entities/Router.ts
+++ src/entities/Router.ts
@@ -62,11 +62,7 @@ export class Router {
         };
     }
 
-    async kybercall(
-        tokenIn: Address,
-        tokenOut: Address,
-        amountIn: BigNumberish
-    ): Promise<KybercallData> {
+    async kybercall(tokenIn: Address, tokenOut: Address, amountIn: BigNumberish): Promise<KybercallData> {
         if (tokenIn.toLowerCase() === tokenOut.toLowerCase()) return { outputAmount: amountIn, encodedSwapData: [] };
         const { data } = await axios.get(KYBER_API[this.chainId], {
             params: { tokenIn, tokenOut, amountIn: BN.from(amountIn).toString(), to: this.contract.address },
