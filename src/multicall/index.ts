a4b1e857f5a24d4ac9c57adbf4c5440b8866ceba
Tran Quang Loc <quangloctran99@gmail.com>
2024-01-16 17:31:29 +0700

    Export Multicall error (#258)

--- src/multicall/index.ts
+++ src/multicall/index.ts
@@ -3,3 +3,4 @@ export * from './MulticallAggregateCaller';
 export * from './Multicall';
 export { Multicall2, PendleMulticallV1, PendleMulticallV2 } from '../contracts/typechainTypes';
 export { Multicall2Abi, PendleMulticallV1Abi, PendleMulticallV2Abi } from '../contracts/abis';
+export * from './errors';
